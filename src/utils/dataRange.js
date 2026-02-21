ddeb0bfce83fda696da67721416e590a7e25b45e
nathanbarretosilveira-dev <nathanbarretosilveira@gmail.com>
2026-02-21 19:12:13 -0300

    Update dataRange.js

--- src/utils/dataRange.js
+++ src/utils/dataRange.js
@@ -22,12 +22,32 @@ const obterHojeBrasilia = () => {
   return formatter.format(new Date());
 };
 
+const obterMesBrasilia = () => {
+  const formatter = new Intl.DateTimeFormat('pt-BR', {
+    timeZone: BRASILIA_TIMEZONE,
+    month: '2-digit'
+  });
+
+  return formatter.format(new Date());
+};
+
 export const getHoje = (dadosPorDia) => {
   if (!dadosPorDia?.length) return [];
 
   const hoje = obterHojeBrasilia();
 
   return dadosPorDia.filter((dia) => normalizarDataDiaMes(dia.data) === hoje);
+};
+
+export const getMesAtual = (dadosPorDia) => {
+  if (!dadosPorDia?.length) return [];
+
+  const mesAtual = obterMesBrasilia();
+
+  return dadosPorDia.filter((dia) => {
+    const [, mes = ''] = normalizarDataDiaMes(dia.data).split('/');
+    return mes === mesAtual;
+  });
 };
 
 export const getSemana = (dadosPorDia) => {
